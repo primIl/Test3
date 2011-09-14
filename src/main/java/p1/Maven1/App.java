f2109e4f08c8dd2a027b33c5c2399db28814bfda
Ivan <my@mail>
2011-09-14 20:26:15 +0300

    2

--- src/main/java/p1/Maven1/App.java
+++ src/main/java/p1/Maven1/App.java
@@ -2,12 +2,10 @@ package p1.Maven1;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
