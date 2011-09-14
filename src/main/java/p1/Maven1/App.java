12f2a6ec0fbe3e40cc27a79e91cdf8b9032542c3
Ivan <my@mail>
2011-09-14 20:25:28 +0300

    init

---
+++ src/main/java/p1/Maven1/App.java
@@ -0,0 +1,13 @@
+package p1.Maven1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/p1/Maven1/AppTest.java
@@ -0,0 +1,12 @@
+package p1.Maven1;
+
+import org.junit.*;
+
+
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		Assert.assertTrue(true);
+	}
+}
